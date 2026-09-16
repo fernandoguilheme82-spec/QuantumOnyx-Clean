1056625e0b3fde5abb4e8b0f49451cd343a1acfb
fernandoguilheme82-spec <fernandoguilheme82@gmail.com>
2026-09-16 18:09:16 -0300

    Integrate Mob Controller into UI

--- main.lua
+++ main.lua
@@ -23,3 +23,21 @@ Tab:CreateButton({
         print("[Desgraça Eclipse] Rayfield funcionando!")
     end,
 })
+
+local AutoFarm = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/fernandoguilheme82-spec/QuantumOnyx-Clean/main/Modules/MobController.lua"
+))()
+
+Tab:CreateToggle({
+    name = "Mob Controller",
+    default = false,
+    callback = function(Value)
+        if Value then
+            AutoFarm:Start()
+            print("[Desgraça Eclipse] Mob Controller: ON")
+        else
+            AutoFarm:Stop()
+            print("[Desgraça Eclipse] Mob Controller: OFF")
+        end
+    end,
+})
