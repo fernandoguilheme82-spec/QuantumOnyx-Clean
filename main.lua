8bfc7f4059a51baa3132483fab974d6c67707d5c
fernandoguilheme82-spec <fernandoguilheme82@gmail.com>
2026-09-16 17:38:52 -0300

    Criar novo main.lua do Desgraca Eclipse

---
+++ main.lua
@@ -0,0 +1,15 @@
+--[[
+                    DESGRAÇA ECLIPSE
+              By Bombix Dominante / Nox
+                    Apoio: Blox Brasil
+                  Direitos autorais: Lucas
+]]--
+
+local DesgracaEclipse = {
+    Name = "Desgraça Eclipse",
+    Version = "1.0.0"
+}
+
+print("[Desgraça Eclipse] Inicializando...")
+
+return DesgracaEclipse
